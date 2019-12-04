43cfc1c87b99cf4544105e63d587163f3eb2f6f0
CircleCI <circleci@circleci>
2019-12-04 09:50:57 +0000

    chore(release): Add 0.9.0 release files [ci skip]

---
+++ out/lib/error/UsageError.js
@@ -0,0 +1,26 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+/**
+ * An error reported when {@link yargs} fails to parse arguments.
+ */
+class UsageError extends Error {
+  /**
+   * Creates a new {@link UsageError} based on an error message and the failing parser.
+   * @param {string} message The error message.
+   * @param {string} help Help text for the failing command.
+   */
+  constructor(message, help) {
+    super(message);
+
+    /**
+     * Help text for the failing command.
+     * @type {String}
+     */
+    this.help = help;
+  }
+}
+exports.default = UsageError;
+//# sourceMappingURL=UsageError.js.map
