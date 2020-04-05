976b7d77a663c4d06914ca0d4a4d24f430b6c666
LukasHechenberger <admin@ls-age.com>
2020-04-05 19:30:43 +0200

    docs: Add missing function docs

--- src/lib/util/fs.js
+++ src/lib/util/fs.js
@@ -2,6 +2,11 @@
 
 import { promises as fsp } from 'fs';
 
+/**
+ * Reads a file and parses it's contents as JSON.
+ * @param {string} path The file to read.
+ * @return {Promise<any>} The parsed file contents.
+ */
 export async function readJson(path) {
   return JSON.parse(await fsp.readFile(path, 'utf8'));
 }
