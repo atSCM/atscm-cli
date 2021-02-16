9ecac3ae37e60f67814295b3a6b842f0d1bc7b3c
CircleCI <circleci@circleci>
2021-02-16 09:09:10 +0000

    chore(release): Add 1.1.2 release files [ci skip]

---
+++ out/lib/util/fs.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.readJson = readJson;
+
+var _fs = require("fs");
+
+/* eslint-disable import/prefer-default-export */
+
+/**
+ * Reads a file and parses it's contents as JSON.
+ * @param {string} path The file to read.
+ * @return {Promise<any>} The parsed file contents.
+ */
+async function readJson(path) {
+  return JSON.parse(await _fs.promises.readFile(path, 'utf8'));
+}
+//# sourceMappingURL=fs.js.map
